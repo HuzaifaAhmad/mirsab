311d639878f8daa51d865a7d422d33e71409eedd
Huzaifa Ahmad <huzi8014@gmail.com>
2018-01-16 23:36:30 -0500

    Init

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -13,15 +12,11 @@ var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
-}
-
-func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", indexHandler)
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
-	fmt.Println("Server Running on Port 8080. ")
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
