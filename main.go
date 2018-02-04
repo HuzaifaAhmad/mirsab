6903a4f156ef78b3a9ec17b677b8a06ee22910d2
Huzaifa Ahmad <huzi8014@gmail.com>
2018-02-03 21:22:21 -0500

    Added about and contact

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 var tpl *template.Template
 
-func init() {
+func main() {
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 	r := mux.NewRouter()
 	r.HandleFunc("/", indexHandler)
